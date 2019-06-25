73186041e0e28d1f23b2f1e4e9f019a69d10a8b2
Jamie Vullo <jvullo@protonmail.com>
2019-06-25 23:02:30 +0000

    updated work in progress scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,29 @@
 class Scraper
   
+  require 'nokogiri'
+require 'open-uri'
+
+
+site = "https://en.wikipedia.org/wiki/UFO_sightings_in_the_United_States"
+
+
+page = Nokogiri::HTML(open(site))
+ #puts page #|| 'nope'
+results = page.css(".nowrap")
+#puts results.count
+
+results.each.with_index(1) do |r, index|
+  puts "#{index} - #{r.text}"
+end
+
+# root =
+# url =
+
+# showpage = Nokogiri::HTML(open(""))
+
+# title
+
+
+"Finished Running Code"
+  
 end
