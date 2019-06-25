d9860949b0ccbfd198af3cfcaf02546b19d3ee1d
Jamie Vullo <jvullo@protonmail.com>
2019-06-25 15:42:52 +0000

    fixed run functionality

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,4 +5,3 @@ class CLI
   end
   
 end
-    
--- lib/environment.rb
+++ lib/environment.rb
@@ -2,13 +2,13 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-require_relative "./cli_project/version"
+require_relative "./CLI_Project/version"
 
 module CLIProject
   class Error < StandardError; end
   # Your code goes here...
 end
 
-require_relative './cli_project/cli'
-require_relative './cli_project/scraper'
+require_relative './cli'
+require_relative './scraper'
 
