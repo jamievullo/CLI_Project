72e45296b4495f817114e43c86e5e1690e752304
Jamie Vullo <jvullo@protonmail.com>
2019-06-27 15:47:26 +0000

    quick save after deleting some commented stuff

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,17 +1,11 @@
 class CLI 
   
-  # Program will display a welcome message with a numbered list of 
-  # dates of UFO sightings across the USA along with some program 
-  # instructions. The user will have the option of selecting the 
-  # particular date of interest by number or exiting the program.
-  # Once selected, the program will display the selected event with 
-  # the city, state, event name, and description of the event along 
-  # with an option to go back to the list of dates to check out 
-  # other events as well.
+ 
+  # the city, state, event name, and description of the event along with an option to go back to the list of dates/cities to check out  other events as well.
   
     def call 
-    input = ""
-    while input != "exit"
+      input = ""
+      while input != "exit"
         #Definitely want to add a speed element here for the welcome message.
         
        "I  Want  to  Believe!!".split("").each {|c| putc c ; sleep 0.20}
