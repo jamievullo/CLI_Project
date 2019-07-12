2934cd7a7c0b9d04c697cd52c04d01b019a7a081
Jamie Vullo <jvullo@protonmail.com>
2019-07-12 16:43:15 +0000

    Fixed some indents for readability

--- lib/CLI_Project/scraper.rb
+++ lib/CLI_Project/scraper.rb
@@ -1,30 +1,27 @@
 class UfoSightingsGem::Scraper
-  
-  
+ 
   def self.scrape_ufo_info
   
     site = "https://en.wikipedia.org/wiki/UFO_sightings_in_the_United_States"
   
     page = Nokogiri::HTML(open(site))
-     
     results = page.css("tr")
-  
     results.each do |object|
-    row_data = object.css('td')
+      row_data = object.css('td')
+     
       if row_data[0] != nil && row_data[1] != nil && row_data[3] != nil
+        
         date = row_data[0].text
         city = row_data[1].text
         state = row_data[2].text
         name = row_data[3].text
         description = row_data[4].text
         
-        UfoSightingsGem::Sighting.create(date, city, state, name, description) unless UfoSightingsGem::Sighting.all.count >= 42 
-          #UfoSightingsGem::Sighting.create(date, city, state, name, description)
-        #end
+        unless UfoSightingsGem::Sighting.all.count >= 42 
+          UfoSightingsGem::Sighting.create(date, city, state, name, description)
+        end
       end
     end
   end
 end
-
-
 
